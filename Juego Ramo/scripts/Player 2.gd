ef0cc071bebf9b5a01360219d2a588d434b4f726
Magdalena-Alvarez <magdalena.alvarez@ing.uchile.cl>
2021-10-19 17:21:15 -0300

    arreglar controles

--- Juego Ramo/scripts/Player 2.gd
+++ Juego Ramo/scripts/Player 2.gd
@@ -16,8 +16,8 @@ var facing_right = true
 
 func _physics_process(_delta):
 	var velocity = Vector2()  # The player's movement vector.
-	velocity.x += Input.get_action_strength("mov_der") - Input.get_action_strength("mov_izq")
-	velocity.y += Input.get_action_strength("mov_abajo") - Input.get_action_strength("mov_arriba")
+	velocity.x += Input.get_action_strength("mov_der2") - Input.get_action_strength("mov_izq2")
+	velocity.y += Input.get_action_strength("mov_abajo2") - Input.get_action_strength("mov_arriba2")
 	if not can_move:
 		velocity = Vector2.ZERO
 	if velocity.length() > 0:
@@ -33,9 +33,9 @@ func _physics_process(_delta):
 	elif velocity.x < 0 :
 	   facing_right = false
 	   playback.travel("izq")
-	elif velocity.x == 0 and Input.is_action_just_released("mov_der") or colision and Input.is_action_pressed("mov_der") or Input.is_action_pressed("mov_der") and Input.is_action_pressed("mov_izq") and playback.get_current_node() == "der" :
+	elif velocity.x == 0 and Input.is_action_just_released("mov_der2") or colision and Input.is_action_pressed("mov_der2") or Input.is_action_pressed("mov_der2") and Input.is_action_pressed("mov_izq2") and playback.get_current_node() == "der" :
 	   playback.travel("der_idle")
-	elif velocity.x == 0 and Input.is_action_just_released("mov_izq") or colision and Input.is_action_pressed("mov_izq") or Input.is_action_pressed("mov_der") and Input.is_action_pressed("mov_izq") and playback.get_current_node() == "izq" :
+	elif velocity.x == 0 and Input.is_action_just_released("mov_izq2") or colision and Input.is_action_pressed("mov_izq2") or Input.is_action_pressed("mov_der2") and Input.is_action_pressed("mov_izq2") and playback.get_current_node() == "izq" :
 	   playback.travel("izq_idle")		
 			
 	if velocity.y > 0 :
@@ -44,10 +44,10 @@ func _physics_process(_delta):
 	elif velocity.y < 0: 	  
 	   playback.travel("arriba")
 	   $BulletSpawn.set_position(Vector2(29.803, 3.357))
-	elif velocity.y == 0 and Input.is_action_just_released("mov_abajo") or colision and Input.is_action_pressed("mov_abajo") or Input.is_action_pressed("mov_abajo") and Input.is_action_pressed("mov_arriba") and playback.get_current_node()== "abajo" :
+	elif velocity.y == 0 and Input.is_action_just_released("mov_abajo2") or colision and Input.is_action_pressed("mov_abajo2") or Input.is_action_pressed("mov_abajo2") and Input.is_action_pressed("mov_arriba2") and playback.get_current_node()== "abajo" :
 		playback.travel("idle")
 		$BulletSpawn.set_position(Vector2(-26.197, 23.357))
-	elif velocity.y == 0 and Input.is_action_just_released("mov_arriba") or colision and Input.is_action_pressed("mov_arriba") or Input.is_action_pressed("mov_abajo") and Input.is_action_pressed("mov_arriba") and playback.get_current_node()== "arriba":
+	elif velocity.y == 0 and Input.is_action_just_released("mov_arriba2") or colision and Input.is_action_pressed("mov_arriba2") or Input.is_action_pressed("mov_abajo2") and Input.is_action_pressed("mov_arriba2") and playback.get_current_node()== "arriba":
 		playback.travel("arriba_idle") 
 		$BulletSpawn.set_position(Vector2(29.803, 3.357))
 	
@@ -69,16 +69,16 @@ func weapon():
 	owner.add_child(b)
 	b.transform = $BulletSpawn.global_transform
 	if facing_right: 
-		if Input.is_action_pressed("mov_abajo") or playback.get_current_node() == "idle" :
+		if Input.is_action_pressed("mov_abajo2") or playback.get_current_node() == "idle" :
 			b.rotation = PI/2
 			
-		elif Input.is_action_pressed("mov_arriba") or playback.get_current_node() == "arriba_idle":
+		elif Input.is_action_pressed("mov_arriba2") or playback.get_current_node() == "arriba_idle":
 			b.rotation = -PI/2
 			
 	else: 		
-		if Input.is_action_pressed("mov_abajo") or playback.get_current_node() == "idle" :
+		if Input.is_action_pressed("mov_abajo2") or playback.get_current_node() == "idle" :
 			b.rotation = PI/2
-		elif Input.is_action_pressed("mov_arriba") or playback.get_current_node() == "arriba_idle":
+		elif Input.is_action_pressed("mov_arriba2") or playback.get_current_node() == "arriba_idle":
 			b.rotation = -PI/2
 		else:
 			b.rotation = PI
