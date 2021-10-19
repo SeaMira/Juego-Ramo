f5d48f74c10065d854aea595b249925e967e6664
Gustavo Varas <gustavo@Vendidos-MacBook-Pro.local>
2021-10-19 16:47:05 -0300

    company_intro

---
+++ Juego Ramo/intro.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("fade in")
+	yield(get_tree().create_timer(5), "timeout")
+	$AnimationPlayer.play("faade out")
+	yield(get_tree().create_timer(3), "timeout")
+	get_tree().change_scene("res://scenes/Menu.tscn")
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
