099cf8391c1612efd5c8eea9f13de16314ee6d14
Magdalena-Alvarez <magdalena.alvarez@ing.uchile.cl>
2021-12-26 23:08:59 -0300

    paso de nivel 1 al 2

---
+++ Juego Ramo/scripts/Main2.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+onready var viewport1 = $Viewports/ViewportContainer1/Viewport1
+onready var viewport2 = $Viewports/ViewportContainer2/Viewport2
+onready var camera1 = $Viewports/ViewportContainer1/Viewport1/Camera2D
+onready var camera2 = $Viewports/ViewportContainer2/Viewport2/Camera2D
+onready var world = $"Viewports/ViewportContainer1/Viewport1/Mapa32"
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	viewport2.world_2d = viewport1.world_2d
+	camera1.target = world.get_node("Player 1")
+	camera2.target = world.get_node("Player 2")
---
+++ Juego Ramo/scripts/Paso.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+var player_cant = 0
+func _on_Paso_body_entered(body):
+	
+	if body.is_in_group("player"):
+		player_cant+=1
+		
+	if player_cant ==2:
+		get_tree().change_scene("res://scenes/main2.tscn")
+	pass # Replace with function body.
+
+
+func _on_Paso_body_exited(body):
+	if body.is_in_group("player"):
+		player_cant-=1
