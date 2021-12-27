b777c070357ab6943591a422cbf3c7c2ad499922
esperanzadiaz311 <espe31199@hotmail.com>
2021-12-27 00:22:59 -0300

    Game Over Terminado

--- Juego Ramo/scripts/GameOver.gd
+++ Juego Ramo/scripts/GameOver.gd
@@ -1,16 +1,18 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimationPlayer.play("gameOver")
+	$VBoxContainer/restart.grab_focus()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_restart_pressed():
+	get_tree().change_scene("res://scenes/Main.tscn") # Replace with function body.
+
+
+func _on_menu_pressed():
+	get_tree().change_scene("res://scenes/Menu.tscn") # Replace with function body.
+
+
+func _on_quit_pressed():
+	get_tree().quit() # Replace with function body.
