24bf65dc3230098ffe168b5d335cb153fa6f166c
esperanzadiaz311 <espe31199@hotmail.com>
2021-12-29 18:21:24 -0300

    Escenas para transición fluída

--- Juego Ramo/scripts/mapa32-5.gd
+++ Juego Ramo/scripts/mapa32-5.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
-export var start_enemies = 3 setget set_enemie
+export var start_enemies = 4 setget set_enemie
 
 func set_enemie(value):
 	start_enemies = value
 	if start_enemies <= 0:
-		get_tree().change_scene("res://scenes/mainFinal.tscn")
+		get_tree().change_scene("res://scenes/finPelea.tscn")
 
